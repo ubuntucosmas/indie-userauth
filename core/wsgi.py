8aa98b8d1a994132ddff55ec6e7f197d1c352407
ubuntucosmas <104551616+ubuntucosmas@users.noreply.github.com>
2024-08-09 14:40:27 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from gunicorn.six.moves.urllib.parse import urlsplit
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
