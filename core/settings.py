d60da7504323b07c080d816429014ad8d5b22be0
ubuntucosmas <cosmasasango12@gmail.com>
2024-08-10 10:13:00 +0300

    staticfiles dir

--- core/settings.py
+++ core/settings.py
@@ -148,6 +148,8 @@ STORAGES = {
 
 
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -183,6 +185,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
