c325e43b63c2a0278f26144ddfc2565808da4d42
ubuntucosmas <cosmasasango12@gmail.com>
2024-08-12 12:12:22 +0300

    code commit

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
 from rest_framework.response import Response
-from .serializers import ChangePasswordSerializer, PasswordResetConfirmSerializer, PasswordResetSerializer, ResetPasswordSerializer, UserSerializer
+from .serializers import ChangePasswordSerializer, PasswordResetConfirmSerializer, PasswordResetSerializer, UserSerializer
 from rest_framework import status
 from rest_framework.authtoken.models import Token 
 from django.contrib.auth import authenticate, logout
