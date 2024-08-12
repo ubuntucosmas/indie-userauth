3c24c96ea8cdbdfa79d3606b94a371094c48da31
ubuntucosmas <cosmasasango12@gmail.com>
2024-08-12 20:57:54 +0300

    code commit

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    # Get the PORT from the environment variable
+        # Get the PORT from the environment variable
     port = os.environ.get('PORT', '8000')  # Default to port 8000 for local development
 
     # Run the development server on 0.0.0.0 and the specified port
@@ -23,3 +23,15 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+
+
+
+
+
+
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
